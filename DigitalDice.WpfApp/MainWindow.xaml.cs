43b060b32bf0cf2ab282c29ac9d721c01632d4e3
name <lgu@gmx.at>
2022-04-14 18:58:28 +0200

    Implement On/Off Button and Start Button and the matrix

--- DigitalDice.WpfApp/MainWindow.xaml.cs
+++ DigitalDice.WpfApp/MainWindow.xaml.cs
@@ -20,9 +20,66 @@ namespace DigitalDice.WpfApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool on = false;
+
+        const int m = 7, n = 5;
+        Rectangle[,] cells = new Rectangle[m,n];
+
+        Random random = new();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+
+        private void OnOffButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!on)
+                CreateMatrix();
+            else
+                RemoveMatrix();
+        }
+
+        private void CreateMatrix()
+        {
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Rectangle cell = new();
+                    cell.Height = Matrix.ActualHeight / m;
+                    cell.Width = Matrix.ActualWidth / n;
+                    cell.Fill = Brushes.Red;
+
+                    Matrix.Children.Add(cell);
+
+                    Canvas.SetLeft(cell, j * Matrix.ActualWidth / n);
+                    Canvas.SetTop(cell, i * Matrix.ActualHeight / m);
+
+                    cells[i, j] = cell;
+
+                    on = true;
+                }
+            }
+        }
+
+        private void RemoveMatrix()
+        {
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Matrix.Children.Remove(cells[i,j]);
+                }
+            }
+
+            on = false;
         }
     }
 }
